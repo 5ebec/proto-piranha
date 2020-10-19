4a9b51e2d9da2ec501205524830b3306071909c5
5ebec <hash.a5ebec@gmail.com>
2020-10-19 18:08:03 +0900

    first commit

---
+++ src/Calender.py
@@ -0,0 +1,79 @@
+# coding: utf-8
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+
+class Calender:
+    def __init__(self):
+        self.scopes = ['https://www.googleapis.com/auth/calendar']
+        self.calId = '0e6bmos2vtuuhma6bop1vkdpqo@group.calendar.google.com'
+        self.year = 2020
+
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens,
+        # and is created automatically when the authorization flow completes
+        # for the first time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.scopes)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+        self.service = build('calendar', 'v3', credentials=creds)
+
+    def add_schedules(self, title, url, schedules):
+        schedules = [schedules[i:i + 5] for i in range(0, len(schedules), 5)]
+        for i, date, period, location, note in schedules:
+            date_time = self.datetime_formatter(date.text, period.text)
+            body = {
+                'summary': f"{title} [第{int(i.text)+1}回]",
+                'location': location.text,
+                'description': f"{note.text}\n{url}",
+                'start': {
+                    'dateTime': date_time[0],
+                    'timeZone': 'Japan',
+                },
+                'end': {
+                    'dateTime': date_time[1],
+                    'timeZone': 'Japan',
+                },
+            }
+            event = self.service.events().insert(
+                calendarId=self.calId, body=body).execute()
+            print(event)
+
+    def datetime_formatter(self, date, period):
+        year = self.year
+        date = list(map(int, date.split('/')))
+        time = self.period2time(int(period))
+        if date[0] <= 3:
+            year += 1
+        start_date = datetime.datetime(year, *date, *time[0]).isoformat()
+        end_date = datetime.datetime(year, *date, *time[1]).isoformat()
+        return [start_date, end_date]
+
+    def period2time(self, period):
+        if period == 1:
+            return [[9, 20], [10, 50]]
+        elif period == 2:
+            return [[11, 00], [12, 30]]
+        elif period == 3:
+            return [[13, 30], [15, 00]]
+        elif period == 4:
+            return [[15, 10], [16, 40]]
+        elif period == 5:
+            return [[16, 50], [18, 20]]
+        elif period == 6:
+            return [[18, 30], [20, 00]]
---
+++ src/main.py
@@ -0,0 +1,70 @@
+# coding: utf-8
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+import re
+
+from src.Calender import Calender
+
+
+def get_retry(url, retry_times=5):
+    for t in range(retry_times + 1):
+        r = requests.get(url)
+        if t < retry_times:
+            try:
+                r.raise_for_status()
+            except requests.exceptions.RequestException as e:
+                print(e)
+                sleep(10)
+                continue
+        return r
+
+
+def get_title(bs, year):
+    return re.sub(
+        r'\s+',
+        ' ',
+        bs.find('h2').text.replace(f"{year}年度", '').strip()
+    )
+
+
+def main():
+    calender = Calender()
+    # base
+    base_url = "https://syllabus.naist.jp"
+    list_url = f"{base_url}/subjects/preview_list"
+
+    # syllabuses list
+    list_response = get_retry(list_url)
+    list_bs = BeautifulSoup(list_response.content, "html.parser")
+    syllabuses = list_bs.select('.w20pr a')
+
+    for syllabus in syllabuses:
+        subject_name = syllabus.text
+        subject_url = base_url + syllabus.get('href')
+        print(subject_name, subject_url)
+        subject_response = get_retry(subject_url)
+        subject_bs = BeautifulSoup(subject_response.content, "html.parser")
+        class_info = subject_bs.select('.tbl01.mB20')[3]
+
+        if "表示可能なデータがありません。" in class_info.text:
+            schedules = subject_bs.select('.tbl01.mB20')[5].select('tr td')
+            if len(schedules) == 1:
+                continue
+            title = get_title(subject_bs, calender.year)
+            calender.add_schedules(title, subject_url, schedules)
+            sleep(1)
+        else:
+            for class_detail in class_info.select('.btn02.w55'):
+                subject_url = base_url + class_detail.get('href')
+                print(subject_url)
+                subject_response = get_retry(subject_url)
+                subject_bs = BeautifulSoup(
+                    subject_response.content, "html.parser")
+                schedules = subject_bs.select(
+                    '.tbl01.mB20')[4].select('tr td')
+                if len(schedules) == 1:
+                    continue
+                title = get_title(subject_bs, calender.year)
+                calender.add_schedules(title, subject_url, schedules)
+                sleep(1)
